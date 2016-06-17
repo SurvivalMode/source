c7e38883569e25d2429c8b8be79991b7e42a251a
SurvivalMode <im@matty.pw>
2016-06-17 20:28:04 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -241,7 +241,7 @@
         name: "basicBot",
         loggedInID: null,
         scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
-        cmdLink: "http://git.io/245Ppg",
+        cmdLink: "http://bit.ly/userCommands",
         chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
