2311974011a14b2fb58290beda375911b1117d51
SurvivalMode <im@matty.pw>
2016-06-18 23:11:00 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -299,7 +299,7 @@
             etaRestriction: false,
             welcome: true,
             opLink: null,
-            rulesLink: null,
+            rulesLink: "http://bit.ly/networkRules",
             themeLink: null,
             fbLink: null,
             youtubeLink: null,
